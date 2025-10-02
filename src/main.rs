1cf01b26d2fd7f2c6e284d1e6f5eb9bbd4dea82b
jolade okunlade <jokunlade@gmail.com>
2025-10-02 15:47:55 +0100

    feat: main cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use crossterm::terminal;
 use std::io;
 use std::io::Read;
+
+struct Cleanup;
+
+impl Drop for Cleanup {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Could not disable raw mode")
+    }
+}
 fn main() {
     terminal::enable_raw_mode().expect("Could not turn on raw mode");
     let mut buf = [0; 1];
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
-    terminal::disable_raw_mode().expect("Could not turn off raw mode");
+    panic!();
+
+    //terminal::disable_raw_mode().expect("Could not turn off raw mode");
 }
