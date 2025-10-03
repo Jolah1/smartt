1e5e520139188ec9bba25a1457df9d415642b57e
jolade okunlade <jokunlade@gmail.com>
2025-10-03 23:59:32 +0100

    feat: error handlimg

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
-use crossterm::terminal;
-use std::io;
-use std::io::Read;
+use crossterm::event::{Event, KeyCode, KeyEvent};
+use crossterm::{event, terminal};
+use std::time::Duration;
 
-struct Cleanup;
+struct CleanUp;
 
-impl Drop for Cleanup {
+impl Drop for CleanUp {
     fn drop(&mut self) {
-        terminal::disable_raw_mode().expect("Could not disable raw mode")
+        terminal::disable_raw_mode().expect("Unable to disable raw mode")
     }
 }
-fn main() {
-    terminal::enable_raw_mode().expect("Could not turn on raw mode");
-    let mut buf = [0; 1];
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
-    panic!();
 
-    //terminal::disable_raw_mode().expect("Could not turn off raw mode");
+fn main() -> crossterm::Result<()> {
+    let _clean_up = CleanUp;
+    terminal::enable_raw_mode()?;
+    loop {
+        if event::poll(Duration::from_millis(500))? {
+            if let Event::Key(event) = event::read()? {
+                match event {
+                    KeyEvent {
+                        code: KeyCode::Char('q'),
+                        modifiers: event::KeyModifiers::NONE,
+                    } => break,
+                    _ => {
+                        //todo
+                    }
+                }
+                println!("{:?}\r", event);
+            };
+        } else {
+            println!("No input yet\r");
+        }
+    }
+    Ok(())
 }
