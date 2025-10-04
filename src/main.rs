b8edae9fe6d69031564e805bc73f13fe238c92be
jolade okunlade <jokunlade@gmail.com>
2025-10-04 14:27:36 +0100

    feat: clear until NewLine

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crossterm::event::*;
 use crossterm::terminal::ClearType;
-use crossterm::{cursor, event, execute, terminal};
+use crossterm::{cursor, event, execute, queue, terminal};
 use std::io::stdout;
 use std::time::Duration;
 
@@ -12,27 +12,31 @@ impl Drop for CleanUp {
     }
 }
 
-struct EditorsContents {
+struct EditorContents {
     content: String,
 }
 
-impl EditorContents {
-    fn new() -> Self {
-        Self {
-            content: String::new(),
+impl io::Write for EditorContents {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match std::str::from_utf8(buf) {
+            Ok(s) => {
+                self.content.push_str(s);
+                Ok(s.len())
+            }
+            Err(_) => Err(io::ErrorKind::WriteZero.into()),
         }
     }
 
-    fn push(&mut self, ch: char) {
-        self.content.push(ch)
-    }
-
-    fn push_str(&mut self, string: &str) {
-        self.content.push_str(string)
+    fn flush(&mut self) -> io::Result<()> {
+        let out = write!(stdout(), "{}", self.content);
+        stdout().flush()?;
+        self.content.clear();
+        out
     }
 }
 struct Output {
     win_size: (usize, usize),
+    editor_contents: EditorContents,
 }
 
 impl Output {
@@ -40,7 +44,10 @@ impl Output {
         let win_size = terminal::size()
             .map(|(x, y)| (x as usize, y as usize))
             .unwrap();
-        Self { win_size }
+        Self {
+            win_size,
+            editor_contents: EditorContents::new(),
+        }
     }
 
     fn clear_screen() -> crossterm::Result<()> {
@@ -48,21 +55,27 @@ impl Output {
         execute!(stdout(), cursor::MoveTo(0, 0))
     }
 
-    fn draw_rows(&self) {
+    fn draw_rows(&mut self) {
         let screen_rows = self.win_size.1;
         for i in 0..screen_rows {
-            println!("~");
+            self.editor_contents.push('~');
+
+            queue!(
+                self.editor_contents,
+                terminal::Clear(ClearType::UntilNewLine)
+            )
+            .unwrap();
             if i < screen_rows - 1 {
-                println!("\r")
+                self.editor_contents.push_str("\r\n");
             }
-            stdout().flush();
         }
     }
 
-    fn refresh_screen(&self) -> crossterm::Result<()> {
-        Self::clear_screen()?;
+    fn refresh_screen(&mut self) -> crossterm::Result<()> {
+        queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))?;
         self.draw_rows();
-        execute!(stdout(), cursor::MoveTo(0, 0))
+        queue!(self.editor_contents, cursor::MoveTo(0, 0), cursor::Show)?;
+        self.editor_contents.flush()
     }
 }
 struct Reader;
@@ -103,7 +116,7 @@ impl Editor {
         Ok(true)
     }
 
-    fn run(&self) -> crossterm::Result<bool> {
+    fn run(&mut self) -> crossterm::Result<bool> {
         self.output.refresh_screen()?;
         self.process_keypress()
     }
@@ -112,8 +125,7 @@ impl Editor {
 fn main() -> crossterm::Result<()> {
     let _clean_up = CleanUp;
     terminal::enable_raw_mode()?;
-
-    let editor = Editor::new();
+    let mut editor = Editor::new();
     while editor.run()? {}
 
     Ok(())
