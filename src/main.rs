17cf719b0ecebe9bb305dbedceb5415fb245db48
jolade okunlade <jokunlade@gmail.com>
2025-10-04 14:38:56 +0100

    feat: added a welcome message

--- src/main.rs
+++ src/main.rs
@@ -57,8 +57,17 @@ impl Output {
 
     fn draw_rows(&mut self) {
         let screen_rows = self.win_size.1;
+        let screen_columns = self.win_size.0;
         for i in 0..screen_rows {
-            self.editor_contents.push('~');
+            if i == screen_rows / 3 {
+                let mut welcome = format!("Smartt Editor --- Version {}", VERSION);
+                if welcome.len() > screen_columns {
+                    welcome.truncate(screen_columns)
+                }
+                self.editor_contents.push_str(&welcome);
+            } else {
+                self.editor_contents.push('~');
+            }
 
             queue!(
                 self.editor_contents,
